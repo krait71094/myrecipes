b3bb1361a2694f0367d84ebfb3d5d9ada23073d4
krait71094 <krait71094@gmail.com>
2016-11-20 16:36:05 +0000

    Add a few comment into the application and routes files for future references

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
- root 'pages#home'
+ root 'pages#home' #This line is to change the root page to be display the myrecipes homepage
  get '/home', to: 'pages#home'
  
 end
